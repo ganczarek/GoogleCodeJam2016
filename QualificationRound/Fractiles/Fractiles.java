6a82768420d69c58cf54890e42113fa8e18d0a38
Rafal Ganczarek <rafal@ganczarek.info>
2016-04-09 17:49:28 +0100

    Fractiles before first submission

---
+++ QualificationRound/Fractiles/Fractiles.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Fractiles {
+    public static void main(String[] args) {
+        final Scanner in = new Scanner(System.in);
+        final int T = in.nextInt();
+        for (int t = 1; t <= T; t++) {
+            final int K = in.nextInt();
+            final int C = in.nextInt();
+            final int S = in.nextInt();
+            final StringBuilder ans = new StringBuilder();
+
+            if (K == 1) {
+                ans.append(1);
+            } else if (S <= K - 1) {
+                ans.append("IMPOSSIBLE");
+            } else {
+                for (int i = 2; i <= K; i++) {
+                    ans.append(i);
+                    ans.append(" ");
+                }
+            }
+            System.out.printf("Case #%d: %s\n", t, ans.toString());
+        }
+    }
+}
